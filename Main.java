5d7eec8a558caf5bb4e059b108527312af08e8d4
ellag7121 <ellag7121@wrdsb.ca>
2020-10-30 19:10:53 +0000

    finished a8q5

--- Main.java
+++ Main.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class Main implements Runnable, ActionListener{
 
-  // Class Variables  
-  
 
+  //generate the initial random number
+  Random rand = new Random();
+  int randomNumber = rand.nextInt(100) + 1;
+
+  // Class Variables  
+  JPanel mainPanel;
+
+  JLabel descriptionLabel;
+  JLabel outputLabel;
+
+  JTextField guessField;
+
+  JButton submitButton;
+  JButton newNumberButton;
 
   // Method to assemble our GUI
   public void run(){
@@ -21,6 +34,44 @@ public class Main implements Runnable, ActionListener{
  
     
 
+    //initialize the class Variables
+    mainPanel = new JPanel();
+    mainPanel.setLayout(null);
+
+    descriptionLabel = new JLabel("Guess the Number between 0 and 100:");
+    outputLabel = new JLabel();
+
+    guessField = new JTextField();
+
+    submitButton = new JButton("Submit");
+    newNumberButton = new JButton("New Number");
+
+    //set size & location
+    descriptionLabel.setBounds(10,10,300,20);
+    outputLabel.setBounds(10,100,300,20);
+
+    guessField.setBounds(10,40,300,20);
+
+    submitButton.setBounds(10,70,120,20);
+    newNumberButton.setBounds(140,70,170,20);
+
+    //add action listeners
+    submitButton.addActionListener(this);
+    newNumberButton.addActionListener(this);
+
+    //add action commands
+    submitButton.setActionCommand("submit");
+    newNumberButton.setActionCommand("newNumber");
+
+    //add to main mainPanel
+    mainPanel.add(descriptionLabel);
+    mainPanel.add(outputLabel);
+    mainPanel.add(guessField);
+    mainPanel.add(submitButton);
+    mainPanel.add(newNumberButton);
+
+    //add mainPanel to frame
+    frame.add(mainPanel);
   }
 
   // method called when a button is pressed
@@ -28,12 +79,29 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    if(command.equals("submit")){ //if the submit button was pressed
+      //store the entered guess in a Variable
+      int guess = Integer.parseInt(guessField.getText());
+
+      if(guess == randomNumber){ //check if number is correct
+        outputLabel.setText("You are correct! Good job!");
+      }else if(guess < randomNumber){ //if the guess is less than the random number
+        outputLabel.setText("Your guess of " + guess + " is too low!"); //tell the user it's too low
+      }else{ //if the guess is more than the random number
+        outputLabel.setText("Your guess of " + guess + " is too high!"); //tell the user it's too high
+      }
+    }else{
+      //choose a new random number
+      randomNumber = rand.nextInt(100) + 1;
+    }
+
   }
 
   // Main method to start our program
   public static void main(String[] args){
     // Creates an instance of our program
     Main gui = new Main();
+
     // Lets the computer know to start it in the event thread
     SwingUtilities.invokeLater(gui);
   }
